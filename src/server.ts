4601b63813f8c5e129e5461d827cf6ac412d5148
elenmagalhaes <fromeleen@gmail.com>
2021-07-01 19:40:10 -0300

    Add and config database; Create user structure using migrations, controller, service and repository; Created client error handler to the requests.

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,26 @@
-import express from 'express';
+import 'reflect-metadata';
+import express, { Request, Response, NextFunction } from 'express';
+import "express-async-errors";
+
+import { router } from './routes';
+import "./database"
 
 const app = express();
 
-app.get("/", (request, response) => {
-    return response.send('Hello GET');
-});
+app.use(express.json());
+app.use(router);
 
-app.post("/teste", (request, response) => {
-    return response.send('Hello POST');
-});
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+    if(err instanceof Error) {
+        return response.status(400).json({
+            error: err.message
+        });
+    }
+
+    return response.status(500).json({
+        status: "error",
+        message: "Internal Server Error"
+    })
+})
 
 app.listen(3000, () => console.log('Server is running'));
